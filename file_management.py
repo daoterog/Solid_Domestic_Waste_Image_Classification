51fa1e9d842208ecef557875c7559b046e8ad2df
Daniel Otero Gomez <doterog320@gmail.com>
2021-09-28 14:36:23 -0500

    add walk_through_dir to file management

--- file_management.py
+++ file_management.py
@@ -146,6 +146,22 @@ def split_images(root='data/', train_size=0.7, test_proportion=0.5):
                     dst_path = os.path.join(category_test_path,image)
                     shutil.copy(src_path, dst_path)
 
+def walk_through_dir(dir_path):
+    """
+    Walks through dir_path returning its contents.
+    Args:
+        dir_path (str): target directory
+    
+    Returns:
+        A print out of:
+        number of subdiretories in dir_path
+        number of images (files) in each subdirectory
+        name of each subdirectory
+    """
+    
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        print(f"There are {len(dirnames)} directories and {len(filenames)} images in '{dirpath}'.")
+
 def storemodel(classifier, param_dict, dataset_key, X, y, path, model_name):
 
     """
