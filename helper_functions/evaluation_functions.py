f1e6b088a3fca4b66d36a62b7ee5eb48222c1791
Daniel Otero Gómez <46933083+daoterog@users.noreply.github.com>
2021-10-28 16:27:30 -0500

    added multiclass_CV and other functions

--- helper_functions/evaluation_functions.py
+++ helper_functions/evaluation_functions.py
@@ -9,6 +9,198 @@ from sklearn.metrics import (auc, precision_score, recall_score, f1_score,
                             average_precision_score, plot_precision_recall_curve,
                             roc_curve)
 
+def across_class_results(y_true, y_pred, fig, ax):
+
+    """
+    Compiles the presicion, recall, and f1 scores of each class into a single 
+    dataframe. It calculates the accuracy score as well.
+
+    Args:
+        y_true: Array of truth labels (must be same shape as y_pred).
+        y_pred: Array of predicted labels (must be same shape as y_true).
+        fig: figure object for plotting.
+        ax: axis object for plotting.
+
+    Output:
+        class_scores: DataFrame compiling results for each class.
+        accuracy: accuracy score.
+    """
+    # Extract the classification report
+    classification_report_dict = classification_report(y_true=y_true,
+                                                    y_pred=y_pred,
+                                                    output_dict=True)
+
+    # Create empty dictionary
+    class_f1_scores = {}
+    class_precision = {}
+    class_recall = {}
+    accuracy = 0
+
+    # Loop through the classification report items
+    for key, value in classification_report_dict.items():
+        if key == 'accuracy':
+            accuracy = value
+            break
+        else:
+            class_f1_scores[CLASSES[int(Decimal(key))]] = value['f1-score']
+            class_precision[CLASSES[int(Decimal(key))]] = value['precision']
+            class_recall[CLASSES[int(Decimal(key))]] = value['recall']
+
+    # Create DataFrame with dictionary
+    class_scores = pd.DataFrame({"class_name": list(class_f1_scores.keys()),
+                            "f1-score": list(class_f1_scores.values()),
+                            'precision': list(class_precision.values()),
+                            'recall': list(class_recall.values())})\
+                            .sort_values("f1-score", ascending=False)
+
+    # f1-score plot
+    scores = ax.barh(range(len(class_scores)), class_scores["f1-score"].values)
+    ax.set_yticks(range(len(class_scores)))
+    ax.set_yticklabels(list(class_scores["class_name"]))
+    ax.set_xlabel("f1-score")
+    ax.set_title("F1-Scores for each Class")
+    ax.invert_yaxis(); # reverse the order
+
+    return class_scores, accuracy
+
+def make_confusion_matrix(y_true, y_pred, fig, ax, accuracy, classes=None, 
+                           norm=False, text_size=10): 
+
+    """
+    Makes a labelled confusion matrix comparing predictions and ground truth labels.
+    If classes is passed, confusion matrix will be labelled, if not, integer class values
+    will be used.
+
+    Args:
+        y_true: Array of truth labels (must be same shape as y_pred).
+        y_pred: Array of predicted labels (must be same shape as y_true).
+        fig: figure object for plotting.
+        ax: axis object for plotting.
+        accuracy: accuracy score calculated in across_class_results.
+        classes: Array of class labels (e.g. string form). If `None`, integer labels are used.
+        figsize: Size of output figure (default=(10, 10)).
+        text_size: Size of output figure text (default=15).
+        norm: normalize values or not (default=False).
+        savefig: save confusion matrix to file (default=False).
+    
+    Returns:
+        A labelled confusion matrix plot comparing y_true and y_pred.
+
+    Example usage:
+        make_confusion_matrix(y_true=test_labels, # ground truth test labels
+                            y_pred=y_preds, # predicted labels
+                            classes=class_names, # array of class label names
+                            figsize=(15, 15),
+                            text_size=10)
+    """  
+    # Create the confustion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    cm_norm = cm.astype("float") / cm.sum(axis=1)[:, np.newaxis] # normalize it
+    n_classes = cm.shape[0] # find the number of classes we're dealing with
+
+    # Plot the figure and make it pretty
+    divider = make_axes_locatable(ax)
+    # cax = divider.append_axes('right', size='5%', pad=0.05)
+    cax = ax.matshow(cm, cmap=plt.cm.Blues) # colors will represent how 'correct' a class is, darker == better
+    fig.colorbar(cax, ax=ax)
+
+    # Are there a list of classes?
+    if classes:
+        labels = classes
+    else:
+        labels = np.arange(cm.shape[0])
+    
+    # Label the axes
+    ax.set(title=f"Confusion Matrix, Overall Accuracy = {accuracy:.3f}",
+            xlabel="Predicted label",
+            ylabel="True label",
+            xticks=np.arange(n_classes), # create enough axis slots for each class
+            yticks=np.arange(n_classes), 
+            xticklabels=labels, # axes will labeled with class names (if they exist) or ints
+            yticklabels=labels)
+    
+    # Make x-axis labels appear on bottom
+    ax.xaxis.set_label_position("bottom")
+    ax.xaxis.tick_bottom()
+
+    # Set the threshold for different colors
+    threshold = (cm.max() + cm.min()) / 2.
+
+    # Plot the text on each cell
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        if norm:
+            ax.text(j, i, f"{cm[i, j]} ({cm_norm[i, j]*100:.1f}%)",
+                horizontalalignment="center",
+                color="white" if cm[i, j] > threshold else "black",
+                size=text_size)
+        else:
+            ax.text(j, i, f"{cm[i, j]}",
+                horizontalalignment="center",
+                color="white" if cm[i, j] > threshold else "black",
+                size=text_size)
+            
+def multiclass_CV(classifier, k, X_dict, y, param_dict, param_title_dictionary, 
+                  model_name, path):
+    
+    """
+    Plots a confusion matrix and f1-scores calculated from the concatenation of 
+    the results of the classifier over the whole dataset.
+
+    Args:
+        classifier: model.
+        k: number of splits.
+        X_dict: dataset dictionary.
+        y: labels.
+        param_dict: parameter dictionary for the model.
+        param_title_dictionary: model hyperparameter title (String).
+        model_name: self explanatory.
+        path: to load the data.
+
+    """
+    
+    for dataset_key in X_dict.keys():
+
+        X = X_dict[dataset_key].to_numpy()
+
+        # Set parameters
+        classifier.set_params(**param_dict[dataset_key])
+
+        # Experiment Title
+        title = model_name + '_' + dataset_key + '_' + \
+                                            param_title_dictionary[dataset_key]
+
+        skf = StratifiedKFold(n_splits=k)
+
+        pred_labels = []
+        true_labels = []
+
+        for train_index, test_index in skf.split(X, y):
+
+            # Split data
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+
+            # Fit the model and predict
+            classifier.fit(X_train, y_train)
+            y_pred = classifier.predict(X_test)
+
+            # Make confusion matrix
+            pred_labels.extend(y_pred)
+            true_labels.extend(y_test)
+
+        fig, ax = plt.subplots(1, 2, figsize=(16, 8), dpi=100)
+
+        class_scores, accuracy = across_class_results(true_labels, pred_labels, 
+                                                fig, ax[1])
+        make_confusion_matrix(true_labels, pred_labels, fig, ax[0], accuracy,
+                               CLASSES, norm=True)
+        fig.suptitle(title)
+
+        # Save Figure
+        filename = 'cv_' + str(title) + '.jpg'
+        plt.savefig(os.path.join(path,filename), dpi = 100)
+        plt.show()
+
 def plotlearningcurve(model_name, param_dict, param_title_dictionary, score, 
                       train_sizes_dict, train_scores_mean_dict, 
                       train_scores_std_dict, test_scores_mean_dict,
